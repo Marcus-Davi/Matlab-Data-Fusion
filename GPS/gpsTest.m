e2e1c9ce6daccab8b28aa216da469d55179be841
Marcus Forte <davi2812@dee.ufc.br>
2020-08-19 23:50:57 -0300

    gps  tests

--- GPS/gpsTest.m
+++ GPS/gpsTest.m
@@ -3,15 +3,15 @@ Freq = 5;
 Lab0 = [-3.743718 -38.577979 0];
 GPS = gpsSensor('UpdateRate',Freq,'ReferenceLocation',Lab0,'HorizontalPositionAccuracy',3,'DecayFactor',0.5);
 
-Samples = 1000;
+Samples = 1;
 trueX = 0*ones(Samples,3);
 trueV = 0*ones(Samples,3);
 
 [p,v,gs,c] = GPS(trueX,trueV);
 
 
-% [x,y] = utm(p(:,1),p(:,2));
-% [lx,ly] = utm(Lab0(1),Lab0(2));
+[x,y] = utm(p(:,1),p(:,2));
+[lx,ly] = utm(Lab0(1),Lab0(2));
 
 % return
 
@@ -22,8 +22,8 @@ fprintf('std_dev vx = %f\n',sqrt(var(v(:,1))))
 
 
 plot(x_eq,y_eq,'*')
-% hold on
-% plot(x-lx,y-ly,'.')
+hold on
+plot(x-lx,y-ly,'.')
 grid on
 figure
 plot(v(:,1))
